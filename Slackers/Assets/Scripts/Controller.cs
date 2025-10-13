995ee0c0f47a3487c35f4c1205fa3670fdd13573
Kian Wootton <kian200351@outlook.com>
2025-10-13 12:51:57 +0100

    dhdu

--- Slackers/Assets/Scripts/Controller.cs
+++ Slackers/Assets/Scripts/Controller.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class Controller : MonoBehaviour
 {
+    public float speed = 5f;
+    
     // Start is called before the first frame update
     void Start()
     {
