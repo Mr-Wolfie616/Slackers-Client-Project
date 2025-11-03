293f80ef78204b612fa6cdea4de3956b126b4741
jackrlad <jackrsmith1@gmail.com>
2025-11-03 12:50:44 +0000

    I'm removing this

--- Assets/Scripts/CPU/CPUBehaviour.cs
+++ Assets/Scripts/CPU/CPUBehaviour.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CPUBehaviour : MonoBehaviour
 {
     public List<CPUJobs> Jobs;
+    private CPUJobs currentJob;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,24 @@ public class CPUBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FindNewJob();
+        //Movement();
+    }
+
+    void FindNewJob()
+    {
+        Dictionary<float, CPUJobs> JobOrder = new Dictionary<float, CPUJobs>();
+        foreach (CPUJobs Job in Jobs)
+        {
+            JobOrder.Add(Job.Priority, Job);
+        }
+
+        currentJob = JobOrder.Max(x => x.Value);
+    }
+
+    void Movement()
+    {
+        transform.LookAt(new Vector3(currentJob.Location.x, transform.position.y, currentJob.Location.z));
+        transform.position += transform.forward * Time.deltaTime;
     }
 }
