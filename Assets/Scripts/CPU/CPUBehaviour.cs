243b48f2e0fe37ba779f354bb505537c9b30b738
jackrlad <jackrsmith1@gmail.com>
2025-11-03 11:43:17 +0000

    I added the CPU INSTANCE

---
+++ Assets/Scripts/CPU/CPUBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPUBehaviour : MonoBehaviour
+{
+    public List<CPUJobs> Jobs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CPU/CPUJobs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "CPU Job", fileName = "New CPU Job")]
+public class CPUJobs : ScriptableObject
+{
+    public Vector3 Location;
+    public float JobLengthInSeconds;
+    public float Priority;
+}
