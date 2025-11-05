0a69946227e58c100bb1ce0e1e20c3dff7ee82cf
jackrlad <jackrsmith1@gmail.com>
2025-11-05 20:54:54 +0000

    Job system all working!!

--- Assets/Scripts/CPU/CPUBehaviour.cs
+++ Assets/Scripts/CPU/CPUBehaviour.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CPUBehaviour : MonoBehaviour
 {
     public List<CPUJobs> AllJobs;
-    
+    public NavMeshAgent Agent;
     private List<CPUJobs> CurrentJobs;
-    private CPUJobs currentJob;
+    private float currentJobTime = 0;
     // Start is called before the first frame update
     void Start()
     {
+        CurrentJobs = new List<CPUJobs>();
         RestockJobs();
     }
 
@@ -32,7 +34,19 @@ public class CPUBehaviour : MonoBehaviour
 
     void Movement()
     {
-        transform.LookAt(new Vector3(currentJob.Location.x, transform.position.y, currentJob.Location.z));
-        transform.position += transform.forward * Time.deltaTime;
+        Vector3 pos = new Vector3(CurrentJobs[0].Location.x, transform.position.y, CurrentJobs[0].Location.z);
+
+        if ((transform.position - pos).magnitude > 1)
+        {
+            Agent.SetDestination(pos);
+            currentJobTime = CurrentJobs[0].JobLengthInSeconds;
+        }
+        else
+        {
+            if (currentJobTime > 0) currentJobTime -= Time.deltaTime;
+            else CurrentJobs.RemoveAt(0);
+        }
+
+        if (CurrentJobs.Count < 1)RestockJobs();
     }
 }
