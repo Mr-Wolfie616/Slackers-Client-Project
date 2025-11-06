918ec3dff5b21e7fa394ad58d1c8ba5e6fbac98c
jackrlad <jackrsmith1@gmail.com>
2025-11-06 12:20:19 +0000

    Aaaaaaand I made the manager!

--- Assets/Scripts/CPU/CPUManager.cs
+++ Assets/Scripts/CPU/CPUManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CPUManager : MonoBehaviour
 {
+    public GameObject CPUPrefab;
+    public int NumberOfCPUs;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateCPUs();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    
+    void GenerateCPUs()
+    {
+        for (int i = 0; i < NumberOfCPUs; i++)
+        {
+            Instantiate(CPUPrefab);
+        }
     }
 }
