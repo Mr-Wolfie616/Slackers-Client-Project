18fc44d0cdb8bdaedc0e718c5bdbb3b14f48fade
Kian Wootton <kian200351@outlook.com>
2025-12-17 00:13:48 +0000

    remove shit

--- Assets/Scripts/Controller/CharacterController.cs
+++ Assets/Scripts/Controller/CharacterController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class Controller : NetworkBehaviour
